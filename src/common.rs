b9bd3af0bebaaa3f424082f599b3ec2e02b0fa0c
Eli Rosenthal <ezr@cs.brown.edu>
2019-08-18 15:15:45 -0700

    rename types to common (take 2)

---
+++ src/common.rs
@@ -0,0 +1,14 @@
+//! This file contains common type definitions and utilities used in other parts of the project.
+pub(crate) type NumTy = u32;
+pub(crate) type NodeIx = petgraph::graph::NodeIndex<NumTy>;
+pub(crate) type Graph<V, E> = petgraph::Graph<V, E, petgraph::Directed, NumTy>;
+pub(crate) type Result<T> = std::result::Result<T, CompileError>;
+
+#[derive(Debug)]
+pub(crate) struct CompileError(pub String);
+
+impl std::fmt::Display for CompileError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
