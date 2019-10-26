c17f166d3da345d626ad22325ff43f3c9725d840
Eli Rosenthal <ezr@cs.brown.edu>
2019-10-25 21:46:00 -0700

    switching computers

--- src/compile.rs
+++ src/compile.rs
@@ -4,6 +4,7 @@ use crate::types::get_types;
 
 pub(crate) fn bytecode<'a,'b>(ctx: cfg::Context<'a, &'b str>) -> (Interp<'a>, Vec<Instr<'a>>) {
     let ts = get_types(ctx.cfg(), ctx.num_idents());
+
     unimplemented!()
 }
 
