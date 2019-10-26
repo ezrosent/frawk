5817ae459f55cfb698cc5bbf557953f86523dae5
Eli Rosenthal <ezr@cs.brown.edu>
2019-10-25 21:45:39 -0700

    switching computers

---
+++ src/compile.rs
@@ -0,0 +1,11 @@
+use crate::bytecode::{Interp,Instr};
+use crate::cfg;
+use crate::types::get_types;
+
+pub(crate) fn bytecode<'a,'b>(ctx: cfg::Context<'a, &'b str>) -> (Interp<'a>, Vec<Instr<'a>>) {
+    let ts = get_types(ctx.cfg(), ctx.num_idents());
+    unimplemented!()
+}
+
+
+pub(crate) fn _test() {}
