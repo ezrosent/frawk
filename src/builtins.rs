f8749bde6530a8fef0a093b1ad26554a37c53fca
Eli Rosenthal <ezr@cs.brown.edu>
2019-10-27 23:35:17 -0700

    about to rip out some code

--- src/builtins.rs
+++ src/builtins.rs
@@ -1,5 +1,7 @@
 //! This module contains definitions and metadata for builtin functions.
 use crate::ast;
+use crate::common::Result;
+use crate::compile;
 use crate::types::{Kind, Propagator, Scalar, SmallVec, TVar};
 use smallvec::smallvec;
 
@@ -24,7 +26,7 @@ static_map!(
 
 impl<'a> TryFrom<&'a str> for Function {
     type Error = (); // error means not found
-    fn try_from(value: &'a str) -> Result<Function, ()> {
+    fn try_from(value: &'a str) -> std::result::Result<Function, ()> {
         match FUNCTIONS.get(value) {
             Some(v) => Ok(*v),
             None => Err(()),
@@ -34,7 +36,7 @@ impl<'a> TryFrom<&'a str> for Function {
 
 impl Function {
     // All builtins are fixed-arity.
-    fn arity_inner(&self) -> usize {
+    pub(crate) fn fixed_arity(&self) -> usize {
         use Function::*;
         match self {
             Hasline | Nextline | Unop(_) => 1,
@@ -42,12 +44,79 @@ impl Function {
             Split => 4, // 3?
         }
     }
+    pub(crate) fn input_ty(
+        &self,
+        incoming: &[compile::Ty],
+    ) -> Result<(SmallVec<compile::Ty>, compile::Ty)> {
+        use {
+            ast::{Binop::*, Unop::*},
+            compile::Ty::*,
+            Function::*,
+        };
+        if incoming.len() != self.fixed_arity() {
+            return err!(
+                "function {} expected {} inputs but got {}",
+                self,
+                self.fixed_arity(),
+                incoming.len()
+            );
+        }
+        Ok(match self {
+            Unop(Neg) | Unop(Pos) => match &incoming[0] {
+                Str | Float => (smallvec![Float], Float),
+                _ => (smallvec![Int], Int),
+            },
+            Unop(Column) => (smallvec![Int], Str),
+            Binop(Concat) => (smallvec![Str; 2], Str),
+            Binop(Match) => (smallvec![Str; 2], Int),
+            // Not doesn't unconditionally convert to integers before negating it. Nonempty strings
+            // are considered "truthy". Floating point numbers are converted beforehand:
+            //    !5 == !1 == 0
+            //    !0 == 1
+            //    !"hi" == 0
+            //    !(0.25) == 1
+            Unop(Not) => match &incoming[0] {
+                Float | Int => (smallvec![Int], Int),
+                Str => (smallvec![Str], Int),
+                _ => return err!("unexpected input to Not: {:?}", incoming),
+            },
+            Binop(LT) | Binop(GT) | Binop(LTE) | Binop(GTE) | Binop(EQ) => (
+                match (incoming[0], incoming[1]) {
+                    (Str, _) | (_, Str) => smallvec![Str; 2],
+                    (Float, _) | (_, Float) => smallvec![Float; 2],
+                    (Int, _) | (_, Int) => smallvec![Int; 2],
+                    _ => return err!("invalid input spec for comparison op: {:?}", &incoming[..]),
+                },
+                Int,
+            ),
+            Binop(Plus) | Binop(Minus) | Binop(Mod) | Binop(Mult) => {
+                match (incoming[0], incoming[1]) {
+                    (Str, _) | (_, Str) | (Float, _) | (_, Float) => (smallvec![Float; 2], Float),
+                    (_, _) => (smallvec![Int; 2], Int),
+                }
+            }
+            Binop(Div) => (smallvec![Float;2], Float),
+            Print => (smallvec![Str; incoming.len()], Int),
+            Nextline => (smallvec![Str], Str),
+            Hasline => (smallvec![Str], Int),
+            // irrelevant return type
+            Setcol => (smallvec![Int, Str], Int),
+            // Split's second input can be a map of either type
+            Split => {
+                if let MapIntStr | MapStrStr = incoming[1] {
+                    (smallvec![Str, incoming[1], Str], Int)
+                } else {
+                    return err!("invalid input spec for split: {:?}", &incoming[..]);
+                }
+            }
+        })
+    }
 
     // Return kind is alway scalar. AWK lets you just assign into a provided map.
     pub(crate) fn signature(&self) -> SmallVec<Kind> {
         match self {
             Function::Split => smallvec![Kind::Scalar, Kind::Map, Kind::Scalar],
-            _ => smallvec![Kind::Scalar; self.arity_inner()],
+            _ => smallvec![Kind::Scalar; self.fixed_arity()],
         }
     }
 }
@@ -55,7 +124,7 @@ impl Function {
 impl Propagator for Function {
     type Item = Scalar;
     fn arity(&self) -> Option<usize> {
-        Some(self.arity_inner())
+        Some(self.fixed_arity())
     }
     fn step(&self, incoming: &[Option<Scalar>]) -> (bool, Option<Scalar>) {
         use {
@@ -164,7 +233,7 @@ impl Variable {
 
 impl<'a> TryFrom<&'a str> for Variable {
     type Error = (); // error means not found
-    fn try_from(value: &'a str) -> Result<Variable, ()> {
+    fn try_from(value: &'a str) -> std::result::Result<Variable, ()> {
         match VARIABLES.get(value) {
             Some(v) => Ok(*v),
             None => Err(()),
