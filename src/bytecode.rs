8002704d8b1cb1546085a761b7a824e78f8484de
Eli Rosenthal <ezr@google.com>
2020-01-25 10:17:25 -0800

    pre-attempt at refactoring bytecode stuff

--- src/bytecode.rs
+++ src/bytecode.rs
@@ -1337,6 +1337,452 @@ impl_all!(runtime::StrMap<'a, Str<'a>>, maps_str_str, MapStrStr, 'a);
 impl_get!(runtime::Iter<Int>, iters_int, IterInt);
 impl_get!(runtime::Iter<Str<'a>>, iters_str, IterStr, 'a);
 
+// Helpful for avoiding big match statements when computing basic walks of the bytecode.
+impl<'a> Instr<'a> {
+    pub(crate) fn accum(&self, mut f: impl FnMut(NumTy, compile::Ty)) {
+        use Instr::*;
+        match self {
+            StoreConstStr(sr, _s) => sr.accum(&mut f),
+            StoreConstInt(ir, _i) => ir.accum(&mut f),
+            StoreConstFloat(fr, _f) => fr.accum(&mut f),
+            IntToStr(sr, ir) => {
+                sr.accum(&mut f);
+                ir.accum(&mut f)
+            }
+            FloatToStr(sr, fr) => {
+                sr.accum(&mut f);
+                fr.accum(&mut f);
+            }
+            StrToInt(ir, sr) => {
+                ir.accum(&mut f);
+                sr.accum(&mut f);
+            }
+            StrToFloat(fr, sr) => {
+                fr.accum(&mut f);
+                sr.accum(&mut f);
+            }
+            FloatToInt(ir, fr) => {
+                ir.accum(&mut f);
+                fr.accum(&mut f);
+            }
+            IntToFloat(fr, ir) => {
+                fr.accum(&mut f);
+                ir.accum(&mut f);
+            }
+            AddInt(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            AddFloat(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            MulInt(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            MulFloat(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            MinusInt(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            MinusFloat(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            ModInt(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            ModFloat(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            Div(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            Not(res, ir) => unimplemented!(),
+            NotStr(res, sr) => unimplemented!(),
+            NegInt(res, ir) => unimplemented!(),
+            NegFloat(res, fr) => unimplemented!(),
+            Concat(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            Match(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            LenStr(res, s) => unimplemented!(),
+            LTFloat(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            LTInt(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            LTStr(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            GTFloat(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            GTInt(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            GTStr(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            LTEFloat(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            LTEInt(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            LTEStr(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            GTEFloat(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            GTEInt(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            GTEStr(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            EQFloat(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            EQInt(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            EQStr(res, l, r) => {
+                res.accum(&mut f);
+                l.accum(&mut f);
+                r.accum(&mut f);
+            }
+            SetColumn(dst, src) => {
+                dst.accum(&mut f);
+                src.accum(&mut f)
+            }
+            GetColumn(dst, src) => {
+                dst.accum(&mut f);
+                src.accum(&mut f)
+            }
+            SplitInt(flds, to_split, arr, pat) => {
+                flds.accum(&mut f);
+                to_split.accum(&mut f);
+                arr.accum(&mut f);
+                pat.accum(&mut f);
+            }
+            SplitStr(flds, to_split, arr, pat) => {
+                flds.accum(&mut f);
+                to_split.accum(&mut f);
+                arr.accum(&mut f);
+                pat.accum(&mut f);
+            }
+            PrintStdout(txt) => txt.accum(&mut f),
+            Print(txt, out, _append) => {
+                txt.accum(&mut f);
+                out.accum(&mut f)
+            }
+            LookupIntInt(res, arr, k) => {
+                res.accum(&mut f);
+                arr.accum(&mut f);
+                k.accum(&mut f)
+            }
+            LookupIntStr(res, arr, k) => {
+                res.accum(&mut f);
+                arr.accum(&mut f);
+                k.accum(&mut f)
+            }
+            LookupIntFloat(res, arr, k) => {
+                res.accum(&mut f);
+                arr.accum(&mut f);
+                k.accum(&mut f)
+            }
+            LookupStrInt(res, arr, k) => {
+                res.accum(&mut f);
+                arr.accum(&mut f);
+                k.accum(&mut f)
+            }
+            LookupStrStr(res, arr, k) => {
+                res.accum(&mut f);
+                arr.accum(&mut f);
+                k.accum(&mut f)
+            }
+            LookupStrFloat(res, arr, k) => {
+                res.accum(&mut f);
+                arr.accum(&mut f);
+                k.accum(&mut f)
+            }
+            ContainsIntInt(res, arr, k) => {
+                res.accum(&mut f);
+                arr.accum(&mut f);
+                k.accum(&mut f)
+            }
+            ContainsIntStr(res, arr, k) => {
+                res.accum(&mut f);
+                arr.accum(&mut f);
+                k.accum(&mut f)
+            }
+            ContainsIntFloat(res, arr, k) => {
+                res.accum(&mut f);
+                arr.accum(&mut f);
+                k.accum(&mut f)
+            }
+            ContainsStrInt(res, arr, k) => {
+                res.accum(&mut f);
+                arr.accum(&mut f);
+                k.accum(&mut f)
+            }
+            ContainsStrStr(res, arr, k) => {
+                res.accum(&mut f);
+                arr.accum(&mut f);
+                k.accum(&mut f)
+            }
+            ContainsStrFloat(res, arr, k) => {
+                res.accum(&mut f);
+                arr.accum(&mut f);
+                k.accum(&mut f)
+            }
+            DeleteIntInt(arr, k) => {
+                arr.accum(&mut f);
+                k.accum(&mut f)
+            }
+            DeleteIntFloat(arr, k) => {
+                arr.accum(&mut f);
+                k.accum(&mut f)
+            }
+            DeleteIntStr(arr, k) => {
+                arr.accum(&mut f);
+                k.accum(&mut f)
+            }
+            DeleteStrInt(arr, k) => {
+                arr.accum(&mut f);
+                k.accum(&mut f)
+            }
+            DeleteStrFloat(arr, k) => {
+                arr.accum(&mut f);
+                k.accum(&mut f)
+            }
+            DeleteStrStr(arr, k) => {
+                arr.accum(&mut f);
+                k.accum(&mut f)
+            }
+            LenIntInt(res, arr) => {
+                res.accum(&mut f);
+                arr.accum(&mut f)
+            }
+            LenIntFloat(res, arr) => {
+                res.accum(&mut f);
+                arr.accum(&mut f)
+            }
+            LenIntStr(res, arr) => {
+                res.accum(&mut f);
+                arr.accum(&mut f)
+            }
+            LenStrInt(res, arr) => {
+                res.accum(&mut f);
+                arr.accum(&mut f)
+            }
+            LenStrFloat(res, arr) => {
+                res.accum(&mut f);
+                arr.accum(&mut f)
+            }
+            LenStrStr(res, arr) => {
+                res.accum(&mut f);
+                arr.accum(&mut f)
+            }
+            StoreIntInt(arr, k, v) => {
+                arr.accum(&mut f);
+                k.accum(&mut f);
+                v.accum(&mut f)
+            }
+            StoreIntFloat(arr, k, v) => {
+                arr.accum(&mut f);
+                k.accum(&mut f);
+                v.accum(&mut f)
+            }
+            StoreIntStr(arr, k, v) => {
+                arr.accum(&mut f);
+                k.accum(&mut f);
+                v.accum(&mut f)
+            }
+            StoreStrInt(arr, k, v) => {
+                arr.accum(&mut f);
+                k.accum(&mut f);
+                v.accum(&mut f)
+            }
+            StoreStrFloat(arr, k, v) => {
+                arr.accum(&mut f);
+                k.accum(&mut f);
+                v.accum(&mut f)
+            }
+            StoreStrStr(arr, k, v) => {
+                arr.accum(&mut f);
+                k.accum(&mut f);
+                v.accum(&mut f)
+            }
+            LoadVarStr(dst, _var) => dst.accum(&mut f),
+            StoreVarStr(_var, src) => src.accum(&mut f),
+            LoadVarInt(dst, _var) => dst.accum(&mut f),
+            StoreVarInt(_var, src) => src.accum(&mut f),
+            LoadVarIntMap(dst, _var) => dst.accum(&mut f),
+            StoreVarIntMap(_var, src) => src.accum(&mut f),
+            IterBeginIntInt(dst, arr) => {
+                dst.accum(&mut f);
+                arr.accum(&mut f)
+            }
+            IterBeginIntFloat(dst, arr) => {
+                dst.accum(&mut f);
+                arr.accum(&mut f)
+            }
+            IterBeginIntStr(dst, arr) => {
+                dst.accum(&mut f);
+                arr.accum(&mut f)
+            }
+            IterBeginStrInt(dst, arr) => {
+                dst.accum(&mut f);
+                arr.accum(&mut f)
+            }
+            IterBeginStrFloat(dst, arr) => {
+                dst.accum(&mut f);
+                arr.accum(&mut f)
+            }
+            IterBeginStrStr(dst, arr) => {
+                dst.accum(&mut f);
+                arr.accum(&mut f)
+            }
+            IterHasNextInt(dst, iter) => {
+                dst.accum(&mut f);
+                iter.accum(&mut f)
+            }
+            IterHasNextStr(dst, iter) => {
+                dst.accum(&mut f);
+                iter.accum(&mut f)
+            }
+            IterGetNextInt(dst, iter) => {
+                dst.accum(&mut f);
+                iter.accum(&mut f)
+            }
+            IterGetNextStr(dst, iter) => {
+                dst.accum(&mut f);
+                iter.accum(&mut f)
+            }
+            MovInt(dst, src) => {
+                dst.accum(&mut f);
+                src.accum(&mut f)
+            }
+            MovFloat(dst, src) => {
+                dst.accum(&mut f);
+                src.accum(&mut f)
+            }
+            MovStr(dst, src) => {
+                dst.accum(&mut f);
+                src.accum(&mut f)
+            }
+            MovMapIntInt(dst, src) => {
+                dst.accum(&mut f);
+                src.accum(&mut f)
+            }
+            MovMapIntFloat(dst, src) => {
+                dst.accum(&mut f);
+                src.accum(&mut f)
+            }
+            MovMapIntStr(dst, src) => {
+                dst.accum(&mut f);
+                src.accum(&mut f)
+            }
+            MovMapStrInt(dst, src) => {
+                dst.accum(&mut f);
+                src.accum(&mut f)
+            }
+            MovMapStrFloat(dst, src) => {
+                dst.accum(&mut f);
+                src.accum(&mut f)
+            }
+            MovMapStrStr(dst, src) => {
+                dst.accum(&mut f);
+                src.accum(&mut f)
+            }
+            ReadErr(dst, file) => {
+                dst.accum(&mut f);
+                file.accum(&mut f)
+            }
+            NextLine(dst, file) => {
+                dst.accum(&mut f);
+                file.accum(&mut f)
+            }
+            ReadErrStdin(dst) => dst.accum(&mut f),
+            NextLineStdin(dst) => dst.accum(&mut f),
+            JmpIf(cond, _lbl) => cond.accum(&mut f),
+            PushInt(reg) => reg.accum(&mut f),
+            PushFloat(reg) => reg.accum(&mut f),
+            PushStr(reg) => reg.accum(&mut f),
+            PushIntInt(reg) => reg.accum(&mut f),
+            PushIntFloat(reg) => reg.accum(&mut f),
+            PushIntStr(reg) => reg.accum(&mut f),
+            PushStrInt(reg) => reg.accum(&mut f),
+            PushStrFloat(reg) => reg.accum(&mut f),
+            PushStrStr(reg) => reg.accum(&mut f),
+            PopInt(reg) => reg.accum(&mut f),
+            PopFloat(reg) => reg.accum(&mut f),
+            PopStr(reg) => reg.accum(&mut f),
+            PopIntInt(reg) => reg.accum(&mut f),
+            PopIntFloat(reg) => reg.accum(&mut f),
+            PopIntStr(reg) => reg.accum(&mut f),
+            PopStrInt(reg) => reg.accum(&mut f),
+            PopStrFloat(reg) => reg.accum(&mut f),
+            PopStrStr(reg) => reg.accum(&mut f),
+            Call(_) | Jmp(_) | Ret | Halt => {}
+        }
+    }
+}
+
 // Used in benchmarking code.
 
 #[cfg(test)]
