d3d27e6b307c27a327ec03530b37ea99a428d0bb
Eli Rosenthal <ezr@cs.brown.edu>
2020-08-16 10:34:17 -0700

    new benchmark runs

--- info/scripts/select.sh
+++ info/scripts/select.sh
@@ -1,14 +1,14 @@
 # Select fields 1,8,19 from the all_train dataset
 MAWK=mawk
 GAWK=gawk
-TSV_UTILS_BIN=./bin
+TSV_UTILS_BIN=../bin
 XSV=xsv
 FRAWK=frawk
 
-CSV1=./all_train.csv
-CSV2=./TREE_GRM_ESTN.csv
-TSV1=./all_train.tsv
-TSV2=./TREE_GRM_ESTN.tsv
+CSV1=../all_train.csv
+CSV2=../TREE_GRM_ESTN.csv
+TSV1=../all_train.tsv
+TSV2=../TREE_GRM_ESTN.tsv
 
 AWK_SCRIPT_CSV='BEGIN { OFS="," } { print $1,$8,$19 }'
 AWK_SCRIPT_TSV='BEGIN { OFS="\t" } { print $1,$8,$19 }'
@@ -23,6 +23,8 @@ for i in {1..5}; do
 	time $GAWK -F'\t' "$AWK_SCRIPT_TSV" "$TSV1" > /dev/null
 	time $FRAWK -icsv --out-file=/dev/null "$AWK_SCRIPT_CSV" "$CSV1" 
 	time $FRAWK -itsv --out-file=/dev/null "$AWK_SCRIPT_TSV" "$TSV1" 
+	time $FRAWK -icsv -pr -j4 --out-file=/dev/null "$AWK_SCRIPT_CSV" "$CSV1" 
+	time $FRAWK -itsv -pr -j4 --out-file=/dev/null "$AWK_SCRIPT_TSV" "$TSV1" 
 	time xsv select 1,8,19  "$CSV1" > /dev/null
 	time xsv select -d'\t' 1,8,19  "$TSV1" > /dev/null
 	time $TSV_UTILS_BIN/tsv-select -f 1,8,19 "$TSV1" > /dev/null
--- info/scripts/summarize.awk
+++ info/scripts/summarize.awk
@@ -1,9 +1,19 @@
 # Summarize the raw output contained in the ".out" files
-$1 == "+" { prog=$2; inp=$NF }
+$1 == "+" {
+	prog=$2; inp=$NF
+	if (/-j4/) {
+		prog="frawk(parallel)"
+	}
+}
 
 $1 == "real" { 
 	key=prog " " inp
-	times[key] = times[key] " " $2
+	real=$2
+	getline
+	user=$2
+	getline
+	sys=$2
+	times[key] = times[key] " " sprintf("%s (%s + %s)", real, user, sys)
 }
 END {
 	for (k in times) {
